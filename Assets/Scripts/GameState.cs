72a54d72cc221d26eab72cb1dc14df5a194a18d4
Unknown <kawaxbart@gmail.com>
2020-01-18 18:49:10 +0100

    Add game state script

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameState
+{
+    public static string playerName1, playerName2;
+    public static int playerScore1, playerScore2;
+
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,26 +6,25 @@ public class UIManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI scorePlayer1;
     [SerializeField] TextMeshProUGUI scorePlayer2;
 
-    int score1 = 0;
-    int score2 = 0;
-
     private void Start()
     {
-        scorePlayer1.text = "" + score1;
-        scorePlayer2.text = "" + score2;
+        GameState.playerScore1 = 0;
+        GameState.playerScore2 = 0;
+        scorePlayer1.text = "" + GameState.playerScore1;
+        scorePlayer2.text = "" + GameState.playerScore1;
     }
 
     public void AddScorePlayer(int player)
     {
         if(player == 1)
         {
-            score1++;
-            scorePlayer1.text = "" + score1;
+            GameState.playerScore1++;
+            scorePlayer1.text = "" + GameState.playerScore1;
         }
         else if (player == 2)
         {
-            score2++;
-            scorePlayer2.text = "" + score2;
+            GameState.playerScore2++;
+            scorePlayer2.text = "" + GameState.playerScore2;
         }
     }
 }
