564d8335c3deaf377bcd4c323ffc96dbd15f9e4e
Ilya <ikalinin911@gmail.com>
2020-01-18 23:06:46 +0100

    WaterActivity

---
+++ Assets/Scripts/Waver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waver : MonoBehaviour
+{
+    private float WaveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WaveSpeed = Random.Range(-1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3 (WaveSpeed*Time.deltaTime, 0);
+    }
+}
