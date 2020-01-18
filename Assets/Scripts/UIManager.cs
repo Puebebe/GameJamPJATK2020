9338ae7cf48be94f4bbdfcc799c46ab4e3b46454
Unknown <kawaxbart@gmail.com>
2020-01-18 19:35:12 +0100

    Update gameplay scene

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,6 +6,13 @@ public class UIManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI scorePlayer1;
     [SerializeField] TextMeshProUGUI scorePlayer2;
 
+    [Header("Game Over Screen")]
+    [SerializeField] GameObject gameOverScreen;
+    [SerializeField] TextMeshProUGUI GOSplayerName1;
+    [SerializeField] TextMeshProUGUI GOSplayerName2;
+    [SerializeField] TextMeshProUGUI GOSplayerScore1;
+    [SerializeField] TextMeshProUGUI GOSplayerScore2;
+
     private void Start()
     {
         GameState.playerScore1 = 0;
@@ -26,5 +33,18 @@ public class UIManager : MonoBehaviour
             GameState.playerScore2++;
             scorePlayer2.text = "" + GameState.playerScore2;
         }
+    }
+
+    public void UpdateGameOverScreen()
+    {
+        GOSplayerName1.text = GameState.playerName1;
+        GOSplayerName2.text = GameState.playerName2;
+        GOSplayerScore1.text = "" + GameState.playerScore1;
+        GOSplayerScore2.text = "" + GameState.playerScore2;
+    }
+
+    public void ShowGameOverScreen()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
