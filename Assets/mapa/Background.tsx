4ef23edf83f5861b0e79ec9b700200aaeca6320d
s16673 <56188105+s16673@users.noreply.github.com>
2020-01-19 02:57:10 +0100

    Wrzucona mapa Tiled [KS]

---
+++ Assets/mapa/Background.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Background" tilewidth="2048" tileheight="2048" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="2048" height="2048" source="../assets/Spirte/Background/bg_iceberg_001.png"/>
+ </tile>
+ <tile id="1">
+  <image width="2048" height="2048" source="../assets/Spirte/Background/niebo_001.png"/>
+ </tile>
+</tileset>
