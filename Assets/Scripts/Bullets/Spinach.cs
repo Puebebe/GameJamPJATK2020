d08edcba502c9e19afe349ec1b0e79632752c5ac
Ilya <ikalinin911@gmail.com>
2020-01-19 08:40:12 +0100

    Spinach

---
+++ Assets/Scripts/Bullets/Spinach.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinach : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            var player = collision.gameObject.GetComponent<Player>();
+            player.transform.localScale *= 2;
+            StartCoroutine(revertPlayerScaleAfterSeconds(3, player));
+
+        }
+        Destroy(gameObject);
+    }
+
+    IEnumerator revertPlayerScaleAfterSeconds(int seconds, Player player)
+    {
+        yield return new WaitForSeconds(seconds);
+        player.transform.localScale /= 2;
+    }
+}
