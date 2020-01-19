950e8f24e94822bd3269c2c5520d93f3ab7c1a98
Ilya <ikalinin911@gmail.com>
2020-01-19 08:29:05 +0100

    Cake && Glue

---
+++ Assets/Scripts/Bullets/Glue.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            var player = collision.gameObject.GetComponent<Player>();
+            player.speed = 0;
+            StartCoroutine(revertPlayerSpeedAfterSeconds(3, player));
+
+        }
+        Destroy(gameObject);
+    }
+
+    IEnumerator revertPlayerSpeedAfterSeconds(int seconds, Player player)
+    {
+        yield return new WaitForSeconds(seconds);
+        player.speed = 700 ;
+    }
+}
