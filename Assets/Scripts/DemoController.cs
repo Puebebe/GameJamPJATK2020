3a7a7fc16cf1e2df194559621caea609c3109910
Unknown <kawaxbart@gmail.com>
2020-01-19 11:01:05 +0100

    Add demo

---
+++ Assets/Scripts/DemoController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class DemoController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject demo;
+    [SerializeField]
+    GameObject canvas;
+
+    float timeWithoutAction;
+    bool demoIsPlaying;
+
+    void Start()
+    {
+        timeWithoutAction = 0;
+        Time.timeScale = 1;
+        demoIsPlaying = false;
+    }
+
+    void Update()
+    {
+        timeWithoutAction += Time.deltaTime;
+
+        if (!demoIsPlaying && timeWithoutAction > 10)
+        {
+            demoIsPlaying = true;
+            //Instantiate(demo);
+            demo.SetActive(true);
+            canvas.SetActive(false);
+        }
+
+        if (Input.anyKey)
+        {
+            timeWithoutAction = 0;
+
+            var vp = GameObject.FindObjectOfType<VideoPlayer>();
+
+            if (vp != null)
+            {
+                demoIsPlaying = false;
+                //Destroy(vp.gameObject);
+                vp.gameObject.SetActive(false);
+                canvas.SetActive(true);
+            }
+        }
+    }
+}
